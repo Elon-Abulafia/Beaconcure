3e8898a31efeea28c377375a5328ffc39f1299b8
Elon-Abulafia <elon945@gmail.com>
2024-03-20 18:39:31 +0200

    Add consts for mongo connection URI

---
+++ consts/mongo.py
@@ -0,0 +1,7 @@
+import os
+
+MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD", "password")
+MONGODB_USER = os.getenv("MONGODB_USER", "user")
+MONGODB_HOST = os.getenv("MONGODB_HOST", "localhost")
+MONGODB_CLUSTER = os.getenv("MONGODB_CLUSTER", "Cluster0")
+MONGODB_URI = f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}/?retryWrites=true&w=majority&appName={MONGODB_CLUSTER}"
